d13a39bd8ca100e6a4803e0858a843ad1df2d23d
Hai Phan <hai.phan@gmail.com>
2021-04-01 10:47:24 -0700

    handle "on the websites THAT you visit" as well

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -8,12 +8,20 @@ function loadExtensions() {
     new Promise(fulfill => chrome.management.getAll(fulfill))
         .then(result => result.filter(ext => ext.type == "extension"))
         .then(also(result => Promise.all(result.map(ext => new Promise(fulfill => chrome.management.getPermissionWarningsById(ext.id, fulfill)).then(x => ext.warnings = x)))))
-        .then(result => result.filter(ext => ext.warnings.includes("Read and change all your data on the websites you visit")))
+        .then(result => result.filter(isRisky))
         .then(result => {
             exts = result.filter(ext => ext.enabled)
             extsd = result.filter(ext => !ext.enabled)
         })
         .catch(console.error)
+}
+
+function isRisky(extension) {
+    const risky = [
+        "Read and change all your data on the websites you visit",
+        "Read and change all your data on the websites that you visit",
+    ]
+    return extension.warnings.some(x => risky.includes(x))
 }
 
 function getExtensionIcon(extension, size) {
